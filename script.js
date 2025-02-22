89d855fb082dd81738839690513f51b9198b10b3
Sabir Hussain <sabirhussain0166@gmail.com>
2025-02-22 11:26:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
               <span class="text-glow">Professional UI/UX & Graphic Designer</span>
             </h1><br>
             <p class="hero-subtitle">Welcome to My Portfolio Website <br> I design modern, minimalist interfaces that engage, inspire, and convert.</p>
-<a href="https://sabirportfolio.github.io/.com/#portfolio" id="portfolio-link">
+<a href="/#portfolio" id="portfolio-link">
   <button type="button" class="portfolio-btn">Open Portfolio</button>
 </a>
 </div>
